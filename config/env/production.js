d09fac2370d29533d946f88f12065df8a7ce0c63
Jason Lewis <J.Lewisg@live.com>
2016-01-16 15:53:49 -0500

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,12 +1,12 @@
 'use strict';
-
+var port = process.env.PORT || 5000;
 module.exports = {
   secure: {
     ssl: true,
     privateKey: './config/sslcerts/key.pem',
     certificate: './config/sslcerts/cert.pem'
   },
-  port: process.env.PORT || 8443,
+  port: process.env.PORT || 5000,
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
