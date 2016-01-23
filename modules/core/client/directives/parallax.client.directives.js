781bfb9587edf18838c0b011982e909e7633cbc3
Jason-LewisG <j.lewisg@live.com>
2016-01-22 23:41:33 -0500

    parallax commit

---
+++ modules/core/client/directives/parallax.client.directives.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('core').directive('cssParallax', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/modules/core/client/directives/parallax.client.directives.html'
+  };
+});
