1ff07a757d87807928b209e3243492590e1199b2
Jason-LewisG <j.lewisg@live.com>
2016-01-24 21:43:40 -0500

    commit

--- modules/core/client/directives/scroll.client.directives.js
+++ modules/core/client/directives/scroll.client.directives.js
@@ -2,25 +2,26 @@
 
 angular.module('core').directive('myScroller', ['$window', function ($window) {
   console.log('Scroll directive initialized. ');
-  return {
-    restrict: 'A',
-    link: function(scope,element,attrs) {
-      angular.element($window).bind('scroll', function () {
-        console.log('this is happening');
+  // return {
+  //   restrict: 'A',
+  //   link: function(scope,element,attrs) {
+  //     angular.element($window).bind('scroll', function () {
 
-        scope.$apply(attrs.myScroller);
-      });
-    }
-  };
-
-  // return function(scope, element, attributes) {
-  //   angular.element($window).bind("scroll", function() {
-  //     if (this.pageYOffset >= 100) {
-  //       console.log('Scrolled below header.');
-  //     } else {
-  //       console.log('Header is in view.');
-  //     }
-  //     scope.$apply();
-  //   });
+  //       scope.$apply(attrs.myScroller);
+        
+  //       console.log('this is happening');
+  //     });
+  //   }
   // };
+
+  return function(scope, element, attributes) {
+    angular.element($window).bind("scroll", function() {
+      if (this.pageYOffset >= 100) {
+        console.log('Scrolled below header.');
+      } else {
+        console.log('Header is in view.');
+      }
+      scope.$apply();
+    });
+  };
 }]);
