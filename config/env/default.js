13709e5b55be6494c1ea0c5efab7d27414767248
Jason-LewisG <j.lewisg@live.com>
2016-01-25 21:02:37 -0500

    commit

--- config/env/default.js
+++ config/env/default.js
@@ -29,7 +29,7 @@ module.exports = {
   sessionKey: 'sessionId',
   sessionCollection: 'sessions',
   logo: 'modules/core/client/img/brand/logo.png',
-  favicon: 'modules/core/client/img/brand/republicanLogo.svg',
+  favicon: 'modules/core/client/img/brand/republicanLogo.png',
   uploads: {
     profileUpload: {
       dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
