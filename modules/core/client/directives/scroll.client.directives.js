9b7b52a88733d133281b929fc8082cb899379bfb
Jason-LewisG <j.lewisg@live.com>
2016-02-03 09:01:58 -0500

    heroku push

--- modules/core/client/directives/scroll.client.directives.js
+++ modules/core/client/directives/scroll.client.directives.js
@@ -7,12 +7,24 @@ angular.module('core').directive('scrolly', function() {
     restrict: 'A',
     link: function(scope, element, attrs) {
       var raw = element[0];
+      var offset;
       console.log('loading element');
 
       element.bind('scroll', function() {
         console.log('in scoll');
         console.log(raw.scrollTop + raw.offsetHeight);
         console.log(raw.scrollHeight);
+
+        offset = raw.scrollTop + raw.offsetHeight;
+        if(offset >= 1148 && offset < 2295) {
+          console.log('inside goals');
+        }
+        if (offset >= 2295 && offset < 3440) {
+          console.log('inside mothods');
+        }
+        if (offset >= 3440) {
+          console.log('inside about');
+        }
 
         if (raw.scrollTop + raw.offsetHeight > raw.scrollHeight) {
           scope.$apply(attrs.scrolly);
