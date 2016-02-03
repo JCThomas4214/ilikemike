85c99230042ac133f8ac7c613c64817d682a5d0e
Jason-LewisG <j.lewisg@live.com>
2016-02-03 14:25:48 -0500

    navbar commit

--- modules/core/client/directives/scroll.client.directives.js
+++ modules/core/client/directives/scroll.client.directives.js
@@ -8,22 +8,34 @@ angular.module('core').directive('scrolly', function() {
     link: function(scope, element, attrs) {
       var raw = element[0];
       var offset;
+      var myEl;
       console.log('loading element');
 
       element.bind('scroll', function() {
-        console.log('in scoll');
         console.log(raw.scrollTop + raw.offsetHeight);
         console.log(raw.scrollHeight);
 
         offset = raw.scrollTop + raw.offsetHeight;
+
         if(offset >= 1148 && offset < 2295) {
+          angular.element( document.querySelector( '#goalsTb' ) ).addClass('active');
           console.log('inside goals');
+        } else {
+          angular.element( document.querySelector( '#goalsTb' ) ).removeClass('active');
         }
+
         if (offset >= 2295 && offset < 3440) {
+          angular.element( document.querySelector( '#methodsTb' ) ).addClass('active');
           console.log('inside mothods');
+        } else {
+          angular.element( document.querySelector( '#methodsTb' ) ).removeClass('active');
         }
+
         if (offset >= 3440) {
+          angular.element( document.querySelector( '#aboutTb' ) ).addClass('active');
           console.log('inside about');
+        } else {
+          angular.element( document.querySelector( '#aboutTb' ) ).removeClass('active');
         }
 
         if (raw.scrollTop + raw.offsetHeight > raw.scrollHeight) {
