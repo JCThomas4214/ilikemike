dc133af9e935e987a6884bac0bf8df08c71677fe
Jason-LewisG <j.lewisg@live.com>
2016-02-06 20:16:41 -0500

    headshot commit

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-    $scope.mike_photo = 'modules/core/client/img/photos/mike_headshot.jpg';
+    $scope.mike_photo = 'modules/core/client/img/photos/mike_headshot_cropped.jpg';
 
     $scope.mission_title = 'Mike Thomas for Florida Senate District 17';
     $scope.mission_head = 'Are You In The District?';
