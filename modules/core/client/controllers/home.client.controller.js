c3ee42f3f6a91cd6fbea12d7309f0690374cdb4c
Jason-LewisG <j.lewisg@live.com>
2016-02-09 12:57:00 -0500

    style commit

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,9 +5,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-    $scope.mike_photo = 'modules/core/client/img/photos/mike_headshot_cropped.jpg';
+    $scope.mike_photo = 'modules/core/client/img/photos/mike_headshot_cropped_2.jpg';
 
-    $scope.mission_title = 'Mike Thomas for Florida Senate District 17';
+    $scope.name = 'Mike Thomas';
+    $scope.name_con = 'for';
+    $scope.name_con2 = 'Florida Senate';
     $scope.mission_head = 'Are You In The District?';
 
   }
