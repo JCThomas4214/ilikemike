0be71927169a68c7055a0bcbe8c787751d24140a
Jason-LewisG <j.lewisg@live.com>
2016-02-20 21:22:43 -0500

    news letter commit

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -2,7 +2,26 @@
 
 var validator = require('validator');
 var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport();
+var generator = require('xoauth2').createXOAuth2Generator({
+    user: 'mikeforflasenate@gmail.com',
+    clientId: '446944948375-v68mcs3mmv22760ph9bkloculunp59mi.apps.googleusercontent.com',
+    clientSecret: '4VTManev5OXkLGyg_yPRgk_M',
+    refreshToken: '1/4ddjdt4h43zb74nrxFy1K3sZL0yk_80t3wgpKa_yFsbBactUREZofsF9C7PrpE-j'
+});
+// listen for token updates
+// you probably want to store these to a db
+generator.on('token', function(token){
+    console.log('New token for %s: %s', token.user, token.accessToken);
+});
+// login
+var transporter = nodemailer.createTransport(({
+    service: 'gmail',
+    auth: {
+      user: 'mikeforflasenate',
+      pass: 'campaign',
+      xoauth2: generator
+    }
+}));
 
 /**
  * Render the main application page
@@ -68,11 +87,27 @@ exports.recieveMail = function (req, res) {
 
   var data = req.body;
 
+  // // setup e-mail data with unicode symbols
+  // var mailOptions = {
+  //   from: data.contact_name + ' <' + data.contact_email + '>', // sender address
+  //   to: 'mikeforflasenate@gmail.com', // list of receivers
+  //   subject: 'New Subscriber: ' + data.contact_name, // Subject line
+  //   text: data.contact_name // plaintext body
+  //   // html: '<b>Hello world 🐴</b>' // html body
+  // };
+
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if(error){
+  //       return console.log(error);
+  //   }
+  //   console.log('Message sent: ' + info.response);
+  // });
+
   transporter.sendMail({
     from: data.contact_email,
-    to: 'jason.lewisg@gmail.com',
+    to: 'mikeforflasenate@gmail.com',
     subject: 'New subscriber',
-    text: data.contact_name
+    text: data.contact_name + ' has just subscribed!'
   });
 
   res.json(data);
@@ -86,11 +121,27 @@ exports.sendMail = function (req, res) {
 
   var data = req.body;
 
+  // // setup e-mail data with unicode symbols
+  // var mailOptions = {
+  //   from: 'Mike Thomas Campaign <mikeforflasenate@gmail.com>', // sender address
+  //   to: data.contact_email, // list of receivers
+  //   subject: 'Mike Thomas for Florida Senate 2016', // Subject line
+  //   text: 'Thank you so much, ' + data.contact_name + ' for subscribing to the Mike Thomas Campaign news Letter!' // plaintext body
+  //   // html: '<b>Hello world 🐴</b>' // html body
+  // };
+
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if(error){
+  //       return console.log(error);
+  //   }
+  //   console.log('Message sent: ' + info.response);
+  // });
+
   transporter.sendMail({
-    from: 'jason.lewisg@gmail.com',
+    from: '"Mike Thomas for Senate" <mikeforflasenate@gmail.com>',
     to: data.contact_email,
     subject: 'Mike Thomas for Florida Senate 2016',
-    text: 'Thank you so much, ' + data.contact_name + ' for subscribing to the Mike Thomas Campaign news Letter!'
+    text: 'Thank you so much, ' + data.contact_name + ', for subscribing to the Mike Thomas Campaign news Letter!'
   });
 
   res.json(data);
