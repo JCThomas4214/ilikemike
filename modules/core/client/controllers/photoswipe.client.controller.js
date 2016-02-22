55e8a37351bfb39bbb65a0375a52e84831062df4
Jason-LewisG <j.lewisg@live.com>
2016-02-22 01:53:57 -0500

    photoswipe fix commit

--- modules/core/client/controllers/photoswipe.client.controller.js
+++ modules/core/client/controllers/photoswipe.client.controller.js
@@ -5,25 +5,32 @@ angular.module('core').controller('photoCtrl', [ '$scope',
 
     $scope.slides1 = [{
       src: 'modules/core/client/img/photos/Mike and Troya 2 2016a.jpg',
-      w: 1277, h: 1242
+      w: 1277, h: 1242,
+      caption: 'Mike and Troya Thomas'
     },{
       src: 'modules/core/client/img/photos/thomas_fam.JPG',
-      w: 640, h: 480
+      w: 640, h: 480,
+      caption: 'Mike\'s immediate family'
     },{
       src: 'modules/core/client/img/photos/Dog Whisperer 8 16 15 - crop.png',
-      w: 506, h: 854
+      w: 506, h: 854,
+      caption: 'Mike the dog whisperer'
     },{
       src: 'modules/core/client/img/photos/Mike & Troya - Lincoln Dinner 3 2010.jpg',
-      w: 1824, h: 1368
+      w: 1824, h: 1368,
+      caption: 'Mr. and Mrs. Thomas at the BREC Lincoln Day Dinner'
     },{
       src: 'modules/core/client/img/photos/Mike w Author.jpg',
-      w: 1296, h: 968
+      w: 1296, h: 968,
+      caption: 'Mike with author Carl...'
     },{
       src: 'modules/core/client/img/photos/Malabar Walk In.jpg',
-      w: 1296, h: 968
+      w: 1296, h: 968,
+      caption: 'Mike is a shareholder and Manager for the Malabar Medical Walk-in Clinic'
     },{
       src: 'modules/core/client/img/photos/Mike NAVY.jpg',
-      w: 466, h: 640
+      w: 466, h: 640,
+      caption: 'Mike when he was enlisted into the NAVY in 1975'
     }];
 
     $scope.slides2 = [{
