fb99d1d5aae749ed339159c8da8c04a752c9c998
Jason-LewisG <j.lewisg@live.com>
2016-02-21 19:19:07 -0500

    endorsements and modularity

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -107,7 +107,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
         text: 'As a parent of three children I wholeheartedly support quality education at all levels. It is incumbent upon local school boards to be inovative in finding ways to enhance the education experience for our children. I pledge not to hobble them with more regulations and red tape that might limit their precious resources. I am supportive of local, instead of centralized, school and education management. The intrusion of "Big Government" with mandated curricula serves to allow misuse of education as a method to indoctrinate our children. An example of this occurred in Brevard County when "approved" texts were criticized when it was demonstrated that the content was skewed to favor certain philosophies.'
       },{
         header: 'Health Care',
-        text: 'After over thirty years in medical practice I have seen the health care needs of people and also witnessed the attempts by the Government to address those needs - most recently in an over reaching and ham handed style. I support getting "Big Government" out of the health care arena and rewriting "Obamacare" to provide a true market based insurance system.  The role of government should be to provide a level playing field environment in which insurers can compete for our business. The artificial "turf" protections that are in place now only serve to stiffle healthy competition. The increased competition will bring down rates for medical care just as it does for other goods and services such as gasoline, food and transportation.'
+        text: 'After over thirty years in medical practice I have seen the health care needs of people and also witnessed the attempts by the Government to address those needs - most recently in an over reaching and ham handed style. I support getting "Big Government" out of the health care arena and rewriting "Obamacare" to provide a true market based insurance system.  The role of government should be to provide a level playing field environment in which insurers can compete for our business. The artificial "turf" protections that are in place now only serve to stifle healthy competition. The increased competition will bring down rates for medical care just as it does for other goods and services such as gasoline, food and transportation.'
       },{
         header: 'Environment',
         text: 'We are blessed with a beautiful piece of the universe. As masters of that real estate it is incumbent upon us to be good stewards of the Earth and its environment. I heartily support laws that would prosecute those who would litter and deface our environment. "Climate Change" is a phrase that has become a political football in recent years. Names get thrown at each other for purposes that are more political than environmental. There is no doubt that climate change is occurring - this is a process that has been occurring since the birth of the earth. There have been substantial climatic fluctuations evident in the geological record. Recently CO2 has been blamed as causing the debatable changes that have been claimed and a whole set of carbon taxes and credits has been instituted. There is evidence that methane gas may be as much as 84 times more potent as a greenhouse gas - and that stuff has been belching from our earth (and cattle) for eons. There are multitudes of other factors that can effect climatic changes as well. It is clear that there is disagreement regarding the whole topic and that further research is warranted. For heaven\'s sake can we just drop the carbon taxes while we try to figure it out?'
@@ -121,13 +121,13 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
     $scope.endorsements = [
       {
-        person: 'George Rego, President of Florida Accademy Physician Assistants',
+        person: 'George Rego, President of Florida Academy Physician Assistants',
         text: '"The FAPA PAC proudly supports Mike Thomas for State Senate. We need Mike up in Tallahassee, he will serve all Floridians admirably."'
       },{
         person: 'Rich Marino, Co-Founder of Space Coast Clinicians',
-        text: '"I\'ve known Mike for over 30 years. He is honest and reliable. I heartily endorse Mike for Florida Senate. He will Serve us all well."'
+        text: '"I\'ve known Mike for over 30 years. He is honest and reliable. I heartily endorse Mike for Florida Senate. He will serve us all well."'
       },{
-        person: 'Joice Brady, Nurse Practitioner',
+        person: 'Joyce O\'Hara Brady, Nurse Practitioner',
         text: '"Honesty, Integrety, Compassion, and amazing Ethics are descriptors of the Mike Thomas I have known professionally for almost 20 years."'
       }
     ];
